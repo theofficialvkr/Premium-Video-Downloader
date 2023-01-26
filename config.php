393d68d6b2d41c14368b2073e4726360b2e01f8a
Vijay Kumar <contactvkr@yahoo.com>
2023-01-26 08:18:10 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(0);
-$serverDomain = "https://vkrfork.ml";
+$serverDomain = "https://vijaykumar.gq";
 $myDomain = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
 $dlAPI = ""; //Downloader Api Key
 $searchAPI = ""; //Search Api Key
